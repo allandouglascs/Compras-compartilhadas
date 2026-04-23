c4352fdb570a7e95e9ed39eb7b337594d36b1b75
allandouglascs <allandouglas18@yahoo.com.br>
2026-04-23 17:28:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = 'https://czjbuakujyslcpiearrn.supabase.co';
+const SUPABASE_URL = 'https://czjbuakujyslcpiearrn.supabaseClient.co';
 const SUPABASE_KEY = 'sb_publishable_ckSMP4wV5PRKQxX7iVYxUg_ikzL1ky6';
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = window.supabaseClient.createClient(SUPABASE_URL, SUPABASE_KEY);
 console.log('script carregou');
 console.log('window.supabase existe?', !!window.supabase);
 console.log('URL:', SUPABASE_URL);
